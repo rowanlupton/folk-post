af24b6c8e0316da34325f2e969d987b11d4da28e
rowan <rowanlupton@gmail.com>
2017-08-20 14:01:05 -0400

    tryna read the environment variable

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ import firebase_admin
 from firebase_admin import credentials, db
 import flask_login
 
-cred = credentials.Base(os.environ['ADMIN_SDK'])
+cred = credentials.Base(os.environ['ADMIN_SDK']).replace("\\n", "\n")
 firebase_admin.initialize_app(cred, { 'databaseURL' : 'https://folk-post.firebaseio.com'})
 
 app = Flask(__name__, template_folder='app/templates')

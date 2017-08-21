7d9f0b647dd41ccff3214676f9ac3633d107a07b
rowan <rowanlupton@gmail.com>
2017-08-20 22:48:58 -0400

    switching to mongodb

--- run.py
+++ run.py
@@ -3,136 +3,100 @@ from flask import Flask, render_template, request, session, flash
 from flask import current_app as current_app
 from app.views import userLogin, userRegister, submitItem, submitItemClaim, submitItemPossessorUpdate, confirmDeleteItem
 from flask_wtf.csrf import CSRFProtect
-import firebase_admin
-from firebase_admin import credentials, db
 import flask_login
+from flask_pymongo import PyMongo
 
-cred = firebase_admin.credentials.Certificate({"type": "service_account",
-																							 "project_id": "folk-post",
-																							 "private_key_id": "4bd8cc2e0acb62daefc8355793eda84e89228450",
-																							 "private_key": os.environ['private_key'],
-																							 "client_email": os.environ['client_email'],
-																							 "client_id": "105602536335211410284",
-																							 "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-																							 "token_uri": "https://accounts.google.com/o/oauth2/token",
-																							 "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-																							 "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-mgffy@folk-post.iam.gserviceaccount.com"})
-firebase_admin.initialize_app(cred, { 'databaseURL' : 'https://folk-post.firebaseio.com'})
 
 app = Flask(__name__, template_folder='app/templates')
 app.config.from_object('config')
-ref = db.reference('')
-items_ref = ref.child('items')
-users_ref = ref.child('users')
 
-
-# login stuff
-login_manager = flask_login.LoginManager()
-login_manager.init_app(app)
-
-class User():
-	#authenticated = users_ref.child().child('authenticated').get()
-
-	def get_id(self):
-		return self.email
-	def is_active(self):
-		return True
-	def is_anonymous(self):
-		return False
-	def is_authenticated(self):
-		return self.authenticated
-
-
-login_manager.login_view = "do_login"
-login_manager.login_message = u"Please log in"
-@login_manager.user_loader
-def load_user(user_id):
-	u = users_ref.query.get(user_id)
-	return User(u.name, u.location)
+uri = 'mongodb://folk-post:'+os.environ['mongodb-password']+'@folk-post-shard-00-00-jwcpw.mongodb.net:27017,folk-post-shard-00-01-jwcpw.mongodb.net:27017,folk-post-shard-00-02-jwcpw.mongodb.net:27017/<DATABASE>?ssl=true&replicaSet=folk-post-shard-0&authSource=admin'
+app.config['MONGO_URI'] = uri
+mongo = PyMongo(app, 'MONGO')
 
 
 # your regularly scheduled app
 @app.route('/')
 def index():
-	results = items_ref.get()
-	return render_template('index.html', results=results)
+	results = mongo.db.items.find()
+	return render_template('index.html', items=results)
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def do_login():
-	form = userLogin()
-	if form.validate_on_submit():
-		if users_ref.child(form.username.data).child('password').get() == form.password.data:
-			flask_login.login_user()
-			return index()
-		else:
-			flash('wrong password')
+# @app.route('/login', methods=['GET', 'POST'])
+# def do_login():
+# 	form = userLogin()
+# 	if form.validate_on_submit():
+# 		if users_ref.child(form.username.data).child('password').get() == form.password.data:
+# 			flask_login.login_user()
+# 			return index()
+# 		else:
+# 			flash('wrong password')
 
-	return render_template('login-page.html', form=form)
+# 	return render_template('login-page.html', form=form)
 
-@app.route('/register', methods=['GET', 'POST'])
-def do_register():
-	form = userRegister()
-	if form.validate_on_submit():
-		if form.password.data == form.passwordConfirm.data:
-			putData = {'password' : form.password.data, 'location' : form.location.data}
-			users_ref.child(form.username.data).set(putData)
-			return render_template('generic-success.html')
-		return "passwords did not match"
-	return render_template('register-page.html', form=form)
+# @app.route('/register', methods=['GET', 'POST'])
+# def do_register():
+# 	form = userRegister()
+# 	if form.validate_on_submit():
+# 		if form.password.data == form.passwordConfirm.data:
+# 			putData = {'password' : form.password.data, 'location' : form.location.data}
+# 			users_ref.child(form.username.data).set(putData)
+# 			return render_template('generic-success.html')
+# 		return "passwords did not match"
+# 	return render_template('register-page.html', form=form)
 
 
 @app.route('/submission', methods=['GET', 'POST'])
-@flask_login.login_required
+# @flask_login.login_required
 def itemSubmission():
 	form = submitItem()
 	if form.validate_on_submit():
 		putData = {'item' : form.item.data, 'description' : form.description.data, 'possessor' : form.possessor.data, 'location' : form.location.data}
-		items_ref.push(putData)
+		mongo.db.itemSubmission.insert_one(putData)
 		return render_template('api-put-result.html', form=form, putData=putData)
 	return render_template('submit-item.html', form=form)
 
 
-@app.route('/user/<name>')
-def viewUser(name):
-	return render_template('user.html')
+# @app.route('/user/<name>')
+# def viewUser(name):
+# 	return render_template('user.html')
 
 
-@app.route('/location/<location>')
-def viewLocation(location):
-	return render_template('location.html')
+# @app.route('/location/<location>')
+# def viewLocation(location):
+# 	return render_template('location.html')
 
 
-@app.route('/items/<key>')
-def viewItem(key):
-	result = items_ref.child(key).get()
-	return render_template('view-item.html', result=result)
+# @app.route('/items/<key>')
+# def viewItem(key):
+# 	result = items_ref.child(key).get()
+# 	return render_template('view-item.html', result=result)
 
 
-@app.route('/items/<key>/claim', methods=['GET', 'POST'])
-def claimItem(key):
-	form = submitItemClaim()
-	if form.validate_on_submit():
-		items_ref.child(key).child('owner').set(form.owner.data)
-		return render_template('generic-success.html')
-	return render_template('claim-item.html', form=form)
+# @app.route('/items/<key>/claim', methods=['GET', 'POST'])
+# def claimItem(key):
+# 	form = submitItemClaim()
+# 	if form.validate_on_submit():
+# 		items_ref.child(key).child('owner').set(form.owner.data)
+# 		return render_template('generic-success.html')
+# 	return render_template('claim-item.html', form=form)
 
 
-@app.route('/<possessor>/<item>/update-possessor', methods=['GET', 'POST'])
-def updateItemPossessor(possessor, item):
-	form = submitItemPossessorUpdate()
-	if form.validate_on_submit():
-		putData = {'possessor' : form.possessor.data}
-		items_ref.child(item).update(putData)
-		return render_template('generic-success.html')
-	return render_template('update-possessor.html', form=form)
+# @app.route('/<possessor>/<item>/update-possessor', methods=['GET', 'POST'])
+# def updateItemPossessor(possessor, item):
+# 	form = submitItemPossessorUpdate()
+# 	if form.validate_on_submit():
+# 		putData = {'possessor' : form.possessor.data}
+# 		items_ref.child(item).update(putData)
+# 		return render_template('generic-success.html')
+# 	return render_template('update-possessor.html', form=form)
 
 
-@app.route('/items/<item>/delete', methods=['GET', 'POST'])
-def deleteItem(item):
-	form = confirmDeleteItem()
-	if form.validate_on_submit():
-		items_ref.child(item).delete()
-		return render_template('generic-success.html')
-	return render_template('confirm-delete-item.html', form=form)
+# @app.route('/items/<item>/delete', methods=['GET', 'POST'])
+# def deleteItem(item):
+# 	form = confirmDeleteItem()
+# 	if form.validate_on_submit():
+# 		items_ref.child(item).delete()
+# 		return render_template('generic-success.html')
+# 	return render_template('confirm-delete-item.html', form=form)
 

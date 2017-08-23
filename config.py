a3b6ffc0b76c0c527ee469bf60db202e4000f294
rowan <rowanlupton@gmail.com>
2017-08-23 19:52:18 +0100

    fixed database URI

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ SECRET_KEY = os.environ['secret_key']
 DB_NAME = 'folk-post'
 
 
-# MONGO_URI = 'mongodb://folk-post:'+os.environ['mongodb-password']+'@folk-post-shard-00-00-jwcpw.mongodb.net:27017,folk-post-shard-00-01-jwcpw.mongodb.net:27017,folk-post-shard-00-02-jwcpw.mongodb.net:27017/<DATABASE>?ssl=true&replicaSet=folk-post-shard-0&authSource=admin'
+MONGO_URI = 'mongodb://folk-post:'+os.environ['mongodb-password']+'@folk-post-shard-00-00-jwcpw.mongodb.net:27017,folk-post-shard-00-01-jwcpw.mongodb.net:27017,folk-post-shard-00-02-jwcpw.mongodb.net:27017/<DATABASE>?ssl=true&replicaSet=folk-post-shard-0&authSource=admin'
 MONGO_DBNAME = 'folk-post'
